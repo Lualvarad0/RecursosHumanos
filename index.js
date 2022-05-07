f015168d01d11f9c0bf809b1e62fe383bf5edb84
HumbertoBaruch <60786173+HumbertoBaruch@users.noreply.github.com>
2022-05-07 11:02:49 -0500

    dependencias

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const morgan = require('morgan');
 const express = require('express');
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running....."); 
